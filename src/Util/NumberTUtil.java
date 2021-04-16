31783dcd4bf1e55694100c9cc2631061a078d2b0
califodream <califodream@what.com>
2021-04-16 13:55:18 +0800

    Fri Apr 16 2021 13:54

--- src/Util/NumberTUtil.java
+++ src/Util/NumberTUtil.java
@@ -3,17 +3,15 @@ package Util;
 public class NumberTUtil {
 
     public static void main(String[] args) {
-        NumberTUtil util = new NumberTUtil();
-        System.out.println(util.fibonacci(45));
+        int[] a = {2, 4, 1, 2, 7, 8, 4};
+        System.out.println(NumberTUtil.distantPeak(a));
     }
 
     /**
      * Memory: 35.6M
      * No Cache
-     * @param n
-     * @return
      */
-    public int fibonacci(int n) {
+    public static int fibonacci(int n) {
         int back = 0;
         int front = 1;
         if (n < 0) return 0;
@@ -27,5 +25,54 @@ public class NumberTUtil {
             index++;
         }
         return front % 1000000007;
+    }
+
+    /**
+     * 找出数组中最大的值的索引
+     */
+    public static int max(int[] numArray) {
+        int tail = numArray.length - 1;
+        int head = 0;
+        while (head < tail) {
+            if (numArray[tail] >= numArray[head])
+                head++;
+            else
+                tail--;
+        }
+        if (numArray[tail] >= numArray[head])
+            return tail;
+        else return head;
+    }
+
+    /**
+     *
+     */
+    public static int distantPeak(int[] numArray) {
+        int head = numArray.length;
+        int tail = -1;
+        int index = head - 1;
+        while (index > -1) {
+            if (index + 1 == head) {
+                if (numArray[index] > numArray[index - 1])
+                    break;
+                else {
+                    index--;
+                    continue;
+                }
+            }
+            if (index - 1 == tail) {
+                if (numArray[index] > numArray[index + 1])
+                    break;
+                else {
+                    index--;
+                    continue;
+                }
+            }
+            if (numArray[index] > numArray[index - 1] && numArray[index] > numArray[index + 1])
+                break;
+            else
+                index--;
+        }
+        return index;
     }
 }
